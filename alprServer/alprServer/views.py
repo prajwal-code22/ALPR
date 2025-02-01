366d9a21fcddc5228064b038f7adced1854138e8
Anuj poudel <anujpoudel54@gmail.com>
2025-02-01 20:24:38 +0545

    added image field in scannedPlate model

--- alprServer/alprServer/views.py
+++ alprServer/alprServer/views.py
@@ -15,26 +15,18 @@ def home(request: HttpRequest):
         lat = request.POST.get("lat")
         uploaded_file = request.FILES.get("image") or request.FILES.get("video")
 
-        file_type = uploaded_file.content_type.split("/")[0].lower()
-
         location, _ = models.Location.objects.get_or_create(long=long, lat=lat)
 
         scanned_plate = models.ScannedPlate(
             locaiont_id=location,
-            user=request.user
+            user=request.user,
+            image = uploaded_file
         )
+
         scanned_plate.save()
         scanned_plate_id = scanned_plate.pk
-
+        file_path = scanned_plate.image.path
         # Handle image here
-        file_extension = uploaded_file.name.split('.')[-1].lower()
-        file_name = f"{file_type}-{scanned_plate_id}-{request.user.pk}.{file_extension}"
-
-        file_path = os.path.join(settings.LICENSE_PLATES, file_name)
-
-        with open(file_path, 'wb+') as destination:
-            for chunk in uploaded_file.chunks():
-                destination.write(chunk)
 
         mat_img = cv2.imread(file_path)
         read_lp_from_image(scanned_plate_id ,mat_img)
