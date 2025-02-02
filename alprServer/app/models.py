c44ad8adf6aeb5f403657b455361493181d69bc7
prajwal <prajwalsiwakoti39@gmail.com>
2025-02-02 16:16:43 +0545

    location changed to location_id in models

--- alprServer/app/models.py
+++ alprServer/app/models.py
@@ -9,7 +9,7 @@ class Location(models.Model):
 
 class ScannedPlate(models.Model):
     user = models.ForeignKey(get_user_model(), related_name="plates", on_delete=models.CASCADE, null=True, blank=True)
-    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="plates")
+    location_id= models.ForeignKey(Location, on_delete=models.CASCADE, related_name="plates")
     scanned_at = models.DateTimeField(auto_now=True)
     image = models.ImageField(upload_to='lp/')
     plate = models.CharField(max_length=200, null=True, blank=True)
